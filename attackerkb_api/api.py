0dd7406419556f3ad8744acb90cb34be47c62d15
Kev <kevthehermit@gmail.com>
2026-01-07 11:30:25 +0000

    Add Ruff and fix the linting

--- attackerkb_api/api.py
+++ attackerkb_api/api.py
@@ -1,6 +1,4 @@
- 
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from __future__ import annotations
 
@@ -10,27 +8,25 @@ from uuid import UUID
 
 import requests
 
-
 ATTACKERKB_API_KEY = "ATTACKERKB_API_KEY"
 
 
 class AttackerKB:
-
     def __init__(self, api_key: str | None = None) -> None:
         self.api_key: str = api_key or os.environ.get(ATTACKERKB_API_KEY, "")
         self.api_base_url: str = "https://api.attackerkb.com/v1/"
         self.api_version: str = "v1"
         self.version: str = "0.0.7"
         self.headers: dict[str, str] = {
-            'Authorization': 'basic ' + self.api_key,
-            'User-Agent': 'AttackerKB-API ' + self.version,
+            "Authorization": "basic " + self.api_key,
+            "User-Agent": "AttackerKB-API " + self.version,
         }
 
         if not self.api_key:
             raise ApiError("You need to provide an attackerkb API key")
 
     def get_topics(self, page: int = 0, size: int = 10, **kwargs: Any) -> list[dict[str, Any]]:
-        """ Get a list of topics that match the kwargs
+        """Get a list of topics that match the kwargs
 
         :param page: An int that set the start page to search from
         :param size: An int that sets how many results per page are returned
@@ -44,11 +40,31 @@ class AttackerKB:
         """
 
         # Strip any invalid kwargs
-        params = {
-            "page": page,
-            "size": size
-        }
-        valid_keys = ["id", "editorId", "name", "created", "createdAfter", "createdBefore", "revisionDate", "revisedAfter", "revisedBefore", "disclosureDate", "document", "metadata", "featured", "rapid7AnalysisCreated", "rapid7AnalysisCreatedAfter", "rapid7AnalysisCreatedBefore", "rapid7AnalysisRevisionDate", "rapid7AnalysisRevisedAfter", "rapid7AnalysisRevisedBefore", "q", "sort", "expand"]
+        params = {"page": page, "size": size}
+        valid_keys = [
+            "id",
+            "editorId",
+            "name",
+            "created",
+            "createdAfter",
+            "createdBefore",
+            "revisionDate",
+            "revisedAfter",
+            "revisedBefore",
+            "disclosureDate",
+            "document",
+            "metadata",
+            "featured",
+            "rapid7AnalysisCreated",
+            "rapid7AnalysisCreatedAfter",
+            "rapid7AnalysisCreatedBefore",
+            "rapid7AnalysisRevisionDate",
+            "rapid7AnalysisRevisedAfter",
+            "rapid7AnalysisRevisedBefore",
+            "q",
+            "sort",
+            "expand",
+        ]
         for key, value in kwargs.items():
             if key in valid_keys:
                 params[key] = value
@@ -56,14 +72,13 @@ class AttackerKB:
                 # Is it worth the raise if you have an invalid argument?
                 pass
 
-        topic_url = '{0}/topics'.format(self.api_base_url)
+        topic_url = f"{self.api_base_url}/topics"
         api_response = requests.get(topic_url, headers=self.headers, params=params)
         result = parse_response(api_response)
         return result
 
-
     def get_single_topic(self, topic_id: str | None = None) -> dict[str, Any]:
-        """ Get a Single topic by its ID
+        """Get a Single topic by its ID
 
         :param topic_id: A Valid UUID for a topic you want to retrieve
 
@@ -72,13 +87,13 @@ class AttackerKB:
         """
         if not topic_id or not valid_uuid(topic_id):
             raise ApiError("You need to provide a valid topic_id")
-        topic_url = '{0}/topics/{1}'.format(self.api_base_url, topic_id)
+        topic_url = f"{self.api_base_url}/topics/{topic_id}"
         api_response = requests.get(topic_url, headers=self.headers)
         result = parse_response(api_response)
         return result
 
     def get_assessments(self, page: int = 0, size: int = 10, **kwargs: Any) -> list[dict[str, Any]]:
-        """ Get a list of assessments that match the kwargs
+        """Get a list of assessments that match the kwargs
 
         :param page: An int that set the start page to search from
         :param size: An int that sets how many results per page are returned
@@ -92,11 +107,24 @@ class AttackerKB:
         """
 
         # Strip any invalid kwargs
-        params = {
-            "page": page,
-            "size": size
-        }
-        valid_keys = ["id", "editorId", "topicId", "created", "createdAfter", "createdBefore", "revisionDate", "revisedAfter", "revisedBefore", "document", "score", "metadata", "q", "sort", "expand"]
+        params = {"page": page, "size": size}
+        valid_keys = [
+            "id",
+            "editorId",
+            "topicId",
+            "created",
+            "createdAfter",
+            "createdBefore",
+            "revisionDate",
+            "revisedAfter",
+            "revisedBefore",
+            "document",
+            "score",
+            "metadata",
+            "q",
+            "sort",
+            "expand",
+        ]
         for key, value in kwargs.items():
             if key in valid_keys:
                 params[key] = value
@@ -104,13 +132,13 @@ class AttackerKB:
                 # Is it worth the raise if you have an invalid argument?
                 pass
 
-        topic_url = '{0}/assessments'.format(self.api_base_url)
+        topic_url = f"{self.api_base_url}/assessments"
         api_response = requests.get(topic_url, headers=self.headers, params=params)
         result = parse_response(api_response)
         return result
 
     def get_single_assessment(self, assessment_id: str) -> dict[str, Any]:
-        """ Get a Single assesment by its ID
+        """Get a Single assesment by its ID
 
         :param assessment_id: A Valid UUID for a topic you want to retrieve
 
@@ -119,19 +147,19 @@ class AttackerKB:
         """
         if not assessment_id or not valid_uuid(assessment_id):
             raise ApiError("You need to provide a valid assessment_id")
-        api_url = '{0}/assessments/{1}'.format(self.api_base_url, assessment_id)
+        api_url = f"{self.api_base_url}/assessments/{assessment_id}"
         api_response = requests.get(api_url, headers=self.headers)
         result = parse_response(api_response)
         return result
 
     def get_contributors(self, page: int = 0, size: int = 10, **kwargs: Any) -> list[dict[str, Any]]:
-        """ Get a list of contributors that match the kwargs
+        """Get a list of contributors that match the kwargs
 
         :param page: An int that set the start page to search from
         :param size: An int that sets how many results per page are returned
         :param **kwargs: A set of Key=Values passed to the function to filter the search.
                          Must be in this list:
-                         ["id", "username", "avatar", "created", "createdAfter", "createdBefore", "score", "q", "sort"] 
+                         ["id", "username", "avatar", "created", "createdAfter", "createdBefore", "score", "q", "sort"]
 
 
         : return: JSON Object with a list of results.
@@ -139,11 +167,8 @@ class AttackerKB:
         """
 
         # Strip any invalid kwargs
-        params = {
-            "page": page,
-            "size": size
-        }
-        valid_keys = ["id", "username" ,"avatar" ,"created", "createdAfter", "createdBefore", "score", "q", "sort"]
+        params = {"page": page, "size": size}
+        valid_keys = ["id", "username", "avatar", "created", "createdAfter", "createdBefore", "score", "q", "sort"]
         for key, value in kwargs.items():
             if key in valid_keys:
                 params[key] = value
@@ -151,14 +176,14 @@ class AttackerKB:
                 # Is it worth the raise if you have an invalid argument?
                 pass
 
-        topic_url = '{0}/contributors'.format(self.api_base_url)
+        topic_url = f"{self.api_base_url}/contributors"
 
         api_response = requests.get(topic_url, headers=self.headers, params=params)
         result = parse_response(api_response)
         return result
 
     def get_single_contributor(self, contributor_id: str) -> dict[str, Any]:
-        """ Get a Single assesment by its ID or Username
+        """Get a Single assesment by its ID or Username
 
         :param assessment_id: A Valid UUID for a topic you want to retrieve or a username string
 
@@ -169,16 +194,14 @@ class AttackerKB:
             raise ApiError("You need to provide a valid contributor_id")
 
         if valid_uuid(contributor_id):
-            api_url = '{0}/contributors/{1}'.format(self.api_base_url, contributor_id)
+            api_url = f"{self.api_base_url}/contributors/{contributor_id}"
             api_response = requests.get(api_url, headers=self.headers)
             result = parse_response(api_response)
             return result
         else:
             # try a username lookup
-            api_url = '{0}/contributors'.format(self.api_base_url)
-            params = {
-                "username": contributor_id
-            }
+            api_url = f"{self.api_base_url}/contributors"
+            params = {"username": contributor_id}
             api_response = requests.get(api_url, headers=self.headers, params=params)
             result = parse_response(api_response)
             return result[0]
@@ -205,7 +228,7 @@ def valid_uuid(uuid_string: str) -> bool:
 
 def parse_response(api_response: requests.Response) -> list[dict[str, Any]] | dict[str, Any]:
     if api_response.status_code == 200:
-        return api_response.json()['data']
+        return api_response.json()["data"]
     elif api_response.status_code == 401:
         raise ApiError("Error Authenticating to the API check your key")
     elif api_response.status_code == 404:
--- tests/test_read.py
+++ tests/test_read.py
@@ -1,6 +1,8 @@
-import pytest
 import os
-from attackerkb_api import AttackerKB, ApiError
+
+import pytest
+
+from attackerkb_api import ApiError, AttackerKB
 
 
 @pytest.fixture
@@ -27,45 +29,45 @@ def test_api_init():
 
 
 def test_single_topic(api):
-    result = api.get_single_topic('6685ce4d-9523-4078-92d3-f08418c9770a')
-    assert result['id'] == '6685ce4d-9523-4078-92d3-f08418c9770a'
+    result = api.get_single_topic("6685ce4d-9523-4078-92d3-f08418c9770a")
+    assert result["id"] == "6685ce4d-9523-4078-92d3-f08418c9770a"
 
 
 def test_search_topic(api):
     result = api.get_topics(name="CVE-2020-10560")
-    assert result[0]['id'] == "6f81bc44-c000-427d-b222-b64c29bda621"
+    assert result[0]["id"] == "6f81bc44-c000-427d-b222-b64c29bda621"
 
 
 def test_search_topic_params(api):
-    result = api.get_assessments(topicId='131226a6-a1e9-48a1-a5d0-ac94baf8dfd2', page=0, size=2, sort="score:asc")
+    result = api.get_assessments(topicId="131226a6-a1e9-48a1-a5d0-ac94baf8dfd2", page=0, size=2, sort="score:asc")
     assert len(result) == 2
 
 
 def test_single_topic_fail(api):
     with pytest.raises(ApiError):
-        api.get_single_topic('not a uuid')
+        api.get_single_topic("not a uuid")
 
 
 def test_single_assesment(api):
-    result = api.get_single_assessment('7c324b6e-0d83-4392-a79f-b61220ebfff3')
-    assert result['id'] == '7c324b6e-0d83-4392-a79f-b61220ebfff3'
+    result = api.get_single_assessment("7c324b6e-0d83-4392-a79f-b61220ebfff3")
+    assert result["id"] == "7c324b6e-0d83-4392-a79f-b61220ebfff3"
 
 
 def test_multi_assesment(api):
-    result = api.get_assessments(topicId='131226a6-a1e9-48a1-a5d0-ac94baf8dfd2')
+    result = api.get_assessments(topicId="131226a6-a1e9-48a1-a5d0-ac94baf8dfd2")
     assert len(result) >= 2
 
 
 def test_single_assesment_fail(api):
     with pytest.raises(ApiError):
-        api.get_single_assessment('not a uuid')
+        api.get_single_assessment("not a uuid")
 
 
 def test_single_user_id(api):
-    result = api.get_single_contributor('7ff62803-e0a8-4121-b324-d4afe9f60d43')
-    assert result['id'] == '7ff62803-e0a8-4121-b324-d4afe9f60d43'
+    result = api.get_single_contributor("7ff62803-e0a8-4121-b324-d4afe9f60d43")
+    assert result["id"] == "7ff62803-e0a8-4121-b324-d4afe9f60d43"
 
 
 def test_single_user_name(api):
-    result = api.get_single_contributor('KevTheHermit')
-    assert result['username'] == 'kevthehermit'
+    result = api.get_single_contributor("KevTheHermit")
+    assert result["username"] == "kevthehermit"
